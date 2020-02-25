8368f90847482e4bc13edddf0933915399efc0bb
Jake Goldsborough <jake.goldsborough@pm.me>
2020-02-24 18:31:56 -0800

    changing types, indenting, moving gitignore up

--- server/src/graphql/models.rs
+++ server/src/graphql/models.rs
@@ -2,34 +2,34 @@ pub mod user {
     #[derive(GraphQLObject)]
     pub struct User {
       pub created: i32,
-      pub account_number: uuid,
+      pub account_number: i32,
     }
 
     #[derive(GraphQLInputObject)]
     pub struct UserCreate {
       pub created: i32,
-      pub account_number: uuid,
+      pub account_number: i32,
     }
 }
 
 pub mod course {
     #[derive(GraphQLObject)]
     pub struct Course {
-        pub id: uuid,
+        pub id: String,
         pub name: String,
         pub city: String,
         pub state: String,
-        pub holes: Option<Vec<Option<Episode>>>,
+        pub holes: Option<Vec<Option<crate::graphql::models::hole::Hole>>>,
         pub par: i32,
     }
 }
 
 pub mod hole {
-    #[derive{GraphQLObject)]
+    #[derive(GraphQLObject)]
     pub struct Hole {
-      pub id: uuid,
+      pub id: String,
       pub distance: i32,
-      pub course_id: uuid,
+      pub course_id: String,
       pub par: i32,
     }
 }
--- server/src/graphql/schema.rs
+++ server/src/graphql/schema.rs
@@ -3,34 +3,44 @@ use juniper::{Context as JuniperContext, FieldResult, RootNode};
 use crate::graphql::models::user::{User, UserCreate};
 use crate::db::Postgres;
 
+use std::fmt;
+
 pub struct Context {
     pub connection: Postgres
 }
 
 impl JuniperContext for Context {}
 
+impl fmt::Debug for Context {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "yo context: {:#?}", self)
+    }
+}
+
 pub struct QueryRoot;
 
 graphql_object!(QueryRoot: Context |&self| {
-  field user(&executor) -> FieldResult<User> {
-    let user = User {
-      created: 1551909311,
-      account_number: 123456789,
-    };
-    Ok(user)
-  }
+    field user(&executor) -> FieldResult<User> {
+        println!("{:?}", executor.context().connection);
+
+        let user = User {
+            created: 1551909311,
+            account_number: 123456789,
+        };
+        Ok(user)
+    }
 });
 
 pub struct MutationRoot;
 
 graphql_object!(MutationRoot: Context |&self| {
-  field add_user(&executor, user_create: UserCreate) -> FieldResult<User> {
-    let user = User {
-      created: user_create.created,
-      account_number: user_create.account_number,
-    };
-    Ok(user)
-  }
+    field add_user(&executor, user_create: UserCreate) -> FieldResult<User> {
+        let user = User {
+            created: user_create.created,
+            account_number: user_create.account_number,
+        };
+        Ok(user)
+    }
 });
 
 pub type Schema = RootNode<'static, QueryRoot, MutationRoot>;
